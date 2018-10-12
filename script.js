5f0655a50f6d86e5b9beab7906d94662d85c8901
[alanna] <[ademuro20@cmc.edu]>
2018-10-12 15:02:44 -0700

    this is it

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
 $(function () {
-    var currSlide = 0;
-    var allSlides = $('section');
-    $(document).on('keydown', function (event) {
-      var nextSlide;
-      if (event.keyCode === 37) { // clicked left arrow
-        nextSlide = currSlide ? currSlide - 1 : currSlide;
-      } else if (event.keyCode === 39) { // clicked right arrow
-        console.log('next slide');
-        nextSlide = currSlide < allSlides.length - 1 ? currSlide + 1 : currSlide;
-      }
-      if (nextSlide !== currSlide) {
-        $(allSlides[currSlide])
-        // a single slide is active, every other slide is inactive and hidden
-          .removeClass('active')
-          .addClass('inactive');
-        $(allSlides[nextSlide])
-          .removeClass('inactive')
-          .addClass('active');
-        currSlide = nextSlide;
-      }
-      event.preventDefault();
-    });
+  'use strict';
+  var currSlide = 1;
+  var allSlides = $('section');
+
+  $(document).on('keydown', function (event) {
+    var nextSlide;
+
+    if (event.keyCode === 37 && currSlide) {
+      console.log('prev slide');
+      $(allSlides[currSlide - 1])
+        .removeClass('prev')
+        .addClass('active');
+      $(allSlides[currSlide + 1])
+        .removeClass('next')
+        .addClass('inactive');
+      $(allSlides[currSlide])
+        .removeClass('active')
+        .addClass('next');
+      $(allSlides[currSlide - 2])
+        .removeClass('inactive')
+        .addClass('prev');
+      currSlide -= 1;
+    } else if (event.keyCode === 39 && currSlide < allSlides.length - 1) {
+      console.log('next slide');
+      $(allSlides[currSlide - 1])
+        .removeClass('prev')
+        .addClass('inactive');
+      $(allSlides[currSlide + 1])
+        .removeClass('next')
+        .addClass('active');
+      $(allSlides[currSlide])
+        .removeClass('active')
+        .addClass('prev');
+      $(allSlides[currSlide + 2])
+        .removeClass('inactive')
+        .addClass('next');
+      currSlide += 1;
+    }
+    event.preventDefault();
   });
-  if (event.keyCode === 37 && currSlide) {
-    // previous slide
-    $(allSlides[currSlide - 1])
-      .removeClass('prev')
-      .addClass('active');
-    $(allSlides[currSlide + 1])
-      .removeClass('next')
-      .addClass('inactive');
-    $(allSlides[currSlide])
-      .removeClass('active')
-      .addClass('next');
-    $(allSlides[currSlide - 2])
-      .removeClass('inactive')
-      .addClass('prev');
-    currSlide -= 1;
-  }
+});
